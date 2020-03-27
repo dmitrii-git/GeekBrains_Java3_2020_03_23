9388323c2e9fe1ca551ebcaca7af952c90b835eb
onolite@gmail.com <123Qwertyimp>
2020-03-27 11:07:30 +0300

    homework lesson2

--- src/Lesson1/homeworkfruit/Apple.java
+++
@@ -1,7 +0,0 @@
-package Lesson1.homeworkfruit;
-
-public class Apple extends Fruit {
-    private final float WEIGHT = 1.0f;
-    private final int FRUITTYPE = 1;
-
-}
--- src/Lesson1/homeworkfruit/Orange.java
+++
@@ -1,7 +0,0 @@
-package Lesson1.homeworkfruit;
-
-public class Orange extends Fruit {
-    private final float WEIGHT = 1.5f;
-    private final int FRUITTYPE = 2;
-
-}
